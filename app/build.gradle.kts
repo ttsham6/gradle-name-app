e3a91a47ad3a75c0efbef36263afa21ee8d4d73a
TTsham6 <37510144+TTsham6@users.noreply.github.com>
2024-01-20 12:01:59 +0900

    gradle設定ファイルでjava17を指定

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,9 @@
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
